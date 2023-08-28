414c41e5128115793ce83426f8986b4296eac319
sakthivel2211 <85622946+sakthivel2211@users.noreply.github.com>
2023-08-28 12:40:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use( express.static( "public" ) );
 
 
 // prepare server
-
 app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
